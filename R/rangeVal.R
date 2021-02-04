6086255a7c0d041354e473878d54d99834b0a2f4
ABSOD <32849428+ABSOD@users.noreply.github.com>
2021-02-04 11:08:19 -0700

    Add files via upload

---
+++ R/rangeVal.R
@@ -0,0 +1,20 @@
+# rangeVal extracts the minimum and maximum from a range of input values
+
+rangeVal <- function(...) {
+
+  x <- list(...)
+  firstval <- unlist(x[1])
+  minValue <- min(firstval)
+  maxValue <- max(firstval)
+  for(i in 2:length(x)){
+    checkval <- unlist(x[i])
+    if(min(checkval) < minValue) {
+       minValue <- min(checkval)
+    }
+    if(max(checkval) > maxValue) {
+      maxValue <- max(checkval)
+    }
+  }
+  outList <- list("min" =  minValue, "max" = maxValue)
+  return(outList)
+}
